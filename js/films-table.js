9eedc049088206f606e29f6ccab420db4581e1cd
Bukarra <ogorodovreg@gmail.com>
2021-05-24 19:55:51 +0300

    ТЗ-16-Таблица с фильмами

--- js/films-table.js
+++ js/films-table.js
@@ -1,5 +1,10 @@
 const films = [
     {
+        start: '01:00',
+        title: 'XXX',
+        adult: true,
+        genre: []
+    },{
         start: '10:00',
         title: 'Человек-паук',
         genre: [
@@ -58,22 +63,46 @@ const films = [
     }
 ];
 
+const filmHelper = {
+    getId() {
+        return this.id || this.title.replaceAll('', '-');
+    },
+    getTitle() {
+        return this.title;
+    },
+    getStart() {
+        return this.start;
+    },
+    getGenre() {
+        return this.genre
+            .map(g => g.name) 
+            .join(', ');
+    },
+}
+
+function renderFilmTableItem(film) {
+    return `
+    <tr>
+        <td>
+            <label class="block03__check">
+                <input type="checkbox" id="${filmHelper.getId.apply(film)}">
+                <svg class="block03__check" width=".6rem" height=".4rem">
+                    <use xlink:href="#check_icon"></use>
+                </svg>
+            </label>
+        </td>
+        <td>${filmHelper.getStart.apply(film)}</td>
+        <td>${filmHelper.getTitle.apply(film)}</td>
+        <td>${filmHelper.getGenre.apply(film)}</td>
+    </tr>
+    `
+}
+
 const tableBody = document.getElementById('block03-table-body');
 tableBody.innerHTML = '';
 
 for (let index = 0; index < films.length; index++) {
-    tableBody.innerHTML += `
-<tr>
-    <td>
-        <a href="#">
-            <svg class="block03__check" width=".6rem" height=".4rem">
-                <use xlink:href="#check_icon"></use>
-            </svg>
-        </a>
-    </td>
-    <td>${films[index].start}</td>
-    <td>${films[index].title}</td>
-    <td>${films[index].genre.map(g => g.name)}</td>
-</tr>
-`;
+    if (!films[index].adult) {
+        tableBody.innerHTML += renderFilmTableItem (films[index]);
+    }
 }
