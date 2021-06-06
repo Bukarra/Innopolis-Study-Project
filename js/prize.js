563c236fb8ea67efe690ac6f0e7f37d61d561115
Bukarra <ogorodovreg@gmail.com>
2021-06-07 01:56:15 +0300

    ТЗ-17-Modal

--- js/prize.js
+++ js/prize.js
@@ -1,11 +1,27 @@
 const openBtn = document.getElementById('btn-prize-open');
 const popup = document.querySelector('#prize-popup');
 const closeBtn = document.querySelector('#prize-popup__close');
+const nameField = document.querySelector('#prize-popup input[name="name"]').parentNode;
+const emailField = document.querySelector('#prize-popup input[name="email"]').parentNode;
 
 
 function popupToggle() {
     popup.classList.toggle('hidden');
 }
+
+const ERROR_CLASS_NAME = 'modal__error';
+const FOCUSED_CLASS_NAME = '';
+
+function initializeField (field) {
+    const input = field.getElementsByTagName('input')[0];
+
+    input.value = '';
+    field.classList.remove(ERROR_CLASS_NAME);
+}
+
+initializeField(nameField);
+initializeField(emailField);
+
 
 openBtn.onclick = popupToggle;
 closeBtn.onclick = popupToggle;
--- target/prize.js
+++ target/prize.js
@@ -3,10 +3,23 @@
 var openBtn = document.getElementById('btn-prize-open');
 var popup = document.querySelector('#prize-popup');
 var closeBtn = document.querySelector('#prize-popup__close');
+var nameField = document.querySelector('#prize-popup input[name="name"]').parentNode;
+var emailField = document.querySelector('#prize-popup input[name="email"]').parentNode;
 
 function popupToggle() {
   popup.classList.toggle('hidden');
 }
 
+var ERROR_CLASS_NAME = 'modal__error';
+var FOCUSED_CLASS_NAME = '';
+
+function initializeField(field) {
+  var input = field.getElementsByTagName('input')[0];
+  input.value = '';
+  field.classList.remove(ERROR_CLASS_NAME);
+}
+
+initializeField(nameField);
+initializeField(emailField);
 openBtn.onclick = popupToggle;
 closeBtn.onclick = popupToggle;
