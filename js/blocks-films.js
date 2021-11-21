84a2190ff4060a3f5d0193f6875fe85112422e36
Bukarra <ogorodovreg@gmail.com>
2021-11-21 13:01:21 +0300

    ТЗ-20-Update2

--- js/blocks-films.js
+++ js/blocks-films.js
@@ -40,16 +40,16 @@ fetchBlockFilms();
 */
 
 fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_AWAIT_FILMS&page=1', {
-    headers: {
-        ...apiHeaders
-    },
-    cors: 'no-cors'
-})
-.then(data => data.json())
-.then(data => {
-    data.films.forEach((film) => {
-        const id = `block-films-desc-${film.filmId}`;
-        blockFilmsWrapper.innerHTML += `
+        headers: {
+            ...apiHeaders
+        },
+        cors: 'no-cors'
+    })
+    .then(data => data.json())
+    .then(data => {
+        data.films.forEach((film) => {
+            const id = `block-films-desc-${film.filmId}`;
+            blockFilmsWrapper.innerHTML += `
             <div class="block05__movie">
                 <img class="block05__pic" src="${film.posterUrlPreview}" alt="Фильм">
                 <div class="block05__shadow"></div>
@@ -64,23 +64,26 @@ fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_AWAIT_FIL
             </div>
         `
 
-        fetch(`https://kinopoiskapiunofficial.tech/api/v2.1/films/${film.filmId}`, {
-            headers: {
-                ...apiHeaders
-            },
-            cors: 'no-cors'
-        })
-        .then(data => data.json())
-        .then(({ data: { description } }) => {
-            const desc = document.getElementById(id);
-            desc.innerText = description;
+            fetch(`https://kinopoiskapiunofficial.tech/api/v2.1/films/${film.filmId}`, {
+                    headers: {
+                        ...apiHeaders
+                    },
+                    cors: 'no-cors'
+                })
+                .then(data => data.json())
+                .then(({
+                    data: {
+                        description
+                    }
+                }) => {
+                    const desc = document.getElementById(id);
+                    desc.innerText = description;
 
-            if (!description) {
-                const root = desc.parentNode.parentNode;
+                    if (!description) {
+                        const root = desc.parentNode.parentNode;
 
-                blockFilmsWrapper.removeChild(root);
-            }
+                        blockFilmsWrapper.removeChild(root);
+                    }
+                })
         })
     })
-})
-
