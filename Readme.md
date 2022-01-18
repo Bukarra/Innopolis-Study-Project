850f5c3a7dcbce1cdc45eeeaa13efaf05de8bad8
Bukarra <ogorodovreg@gmail.com>
2022-01-18 16:50:24 +0300

    ReadmeAdd

---
+++ Readme.md
@@ -0,0 +1,15 @@
+```js
+git add .
+```
+
+```js
+git commit -m "Готова структура index.html"
+```
+
+```js
+git push origin main
+```
+
+```js
+npx babel js -d target --watch
+```
--- js/blocks-films.js
+++ js/blocks-films.js
@@ -44,12 +44,12 @@ fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_AWAIT_FIL
             ...apiHeaders
         },
         cors: 'no-cors'
-    })
-    .then(data => data.json())
-    .then(data => {
-        data.films.forEach((film) => {
-            const id = `block-films-desc-${film.filmId}`;
-            blockFilmsWrapper.innerHTML += `
+})
+.then(data => data.json())
+.then(data => {
+    data.films.forEach((film) => {
+        const id = `block-films-desc-${film.filmId}`;
+        blockFilmsWrapper.innerHTML += `
             <div class="block05__movie">
                 <img class="block05__pic" src="${film.posterUrlPreview}" alt="Фильм">
                 <div class="block05__shadow"></div>
@@ -64,26 +64,20 @@ fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_AWAIT_FIL
             </div>
         `
 
-            fetch(`https://kinopoiskapiunofficial.tech/api/v2.1/films/${film.filmId}`, {
-                    headers: {
-                        ...apiHeaders
-                    },
-                    cors: 'no-cors'
-                })
-                .then(data => data.json())
-                .then(({
-                    data: {
-                        description
-                    }
-                }) => {
-                    const desc = document.getElementById(id);
-                    desc.innerText = description;
-
-                    if (!description) {
-                        const root = desc.parentNode.parentNode;
-
-                        blockFilmsWrapper.removeChild(root);
-                    }
-                })
+        fetch(`https://kinopoiskapiunofficial.tech/api/v2.1/films/${film.filmId}`, {
+            headers: {
+                ...apiHeaders
+            },
+            cors: 'no-cors'
+        })
+        .then(data => data.json())
+        .then(({ data: { description }}) => {
+            const desc = document.getElementById(id);
+            desc.innerText = description
+            if (!description) {
+                const root = desc.parentNode.parentNode
+                blockFilmsWrapper.removeChild(root);
+            }
         })
     })
+})
